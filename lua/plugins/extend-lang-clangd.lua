5fee4fdf5b1b21dd84378b3b8d0f5b8177b7aec2
ddtz <ddtz32@icloud.com>
2025-05-09 18:11:46 +0800

    add keybinding to show c++ type hierarchy

--- lua/plugins/extend-lang-clangd.lua
+++ lua/plugins/extend-lang-clangd.lua
@@ -1,6 +1,15 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = { servers = { clangd = { mason = false } } },
+    opts = {
+      servers = {
+        clangd = {
+          mason = false,
+          keys = {
+            { "<leader>cH", "<cmd>ClangdTypeHierarchy<cr>", desc = "Type Hierarchy" },
+          },
+        },
+      },
+    },
   },
 }
