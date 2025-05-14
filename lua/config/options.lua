194056a81ece25094716499ad4aca3893a224888
ddtz <ddtz32@icloud.com>
2025-05-14 20:26:47 +0800

    disable animation globally

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,7 @@
 -- Show the current document symbols location from Trouble in lualine
 -- You can disable this for a buffer by setting `vim.b.trouble_lualine = false`
 vim.g.trouble_lualine = false
+
+-- Snacks animations
+-- Set to `false` to globally disable all snacks animations
+vim.g.snacks_animate = false
--- lua/plugins/extend-snacks.lua
+++ lua/plugins/extend-snacks.lua
@@ -34,7 +34,6 @@ return {
     },
   },
   opts = {
-    scroll = { enabled = false },
     picker = {
       sources = {
         explorer = {
