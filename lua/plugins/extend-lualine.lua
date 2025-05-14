f65599ddd1fb56261c71a1180480c85705b06664
ddtz <ddtz32@icloud.com>
2025-05-14 22:19:25 +0800

    show full path in lualine

--- lua/plugins/extend-lualine.lua
+++ lua/plugins/extend-lualine.lua
@@ -1,12 +1,26 @@
 return {
   "nvim-lualine/lualine.nvim",
-  opts = function(_, opts)
-    opts.sections = opts.sections or {}
-    -- opts.sections.lualine_z = opts.sections.lualine_y
-    opts.sections.lualine_y = { "filetype" }
-    opts.sections.lualine_z = {
-      { "progress", separator = " ", padding = { left = 1, right = 0 } },
-      { "location", padding = { left = 0, right = 1 } },
-    }
-  end,
+  opts = {
+    sections = {
+      -- copied from lazyvim, then add customized configuration
+      lualine_c = {
+        LazyVim.lualine.root_dir(),
+        {
+          "diagnostics",
+          symbols = {
+            error = LazyVim.config.icons.diagnostics.Error,
+            warn = LazyVim.config.icons.diagnostics.Warn,
+            info = LazyVim.config.icons.diagnostics.Info,
+            hint = LazyVim.config.icons.diagnostics.Hint,
+          },
+        },
+        { LazyVim.lualine.pretty_path({ length = 0 }) },
+      },
+      lualine_y = { "filetype" },
+      lualine_z = {
+        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+        { "location", padding = { left = 0, right = 1 } },
+      },
+    },
+  },
 }
