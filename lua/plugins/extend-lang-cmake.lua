5af1c14e4df0fc6831a953098d2b529087bfd5ca
ddtz <ddtz32@icloud.com>
2025-06-11 20:51:27 +0800

    support cmakelint

--- lua/plugins/extend-lang-cmake.lua
+++ lua/plugins/extend-lang-cmake.lua
@@ -3,4 +3,15 @@ return {
     "stevearc/conform.nvim",
     opts = { formatters_by_ft = { cmake = { "cmakelang" } } },
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = { servers = { neocmake = { init_options = { lint = { enable = false } } } } },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, _)
+      local cmake = require("lint").linters.cmakelint
+      table.insert(cmake.args, "--linelength=120")
+    end,
+  },
 }
